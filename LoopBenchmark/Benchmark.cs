5e26fd744ec5e6232e44f83a7999a1889017f0de
Mike Treit <mtreit@ntdev.microsoft.com>
2021-02-11 17:33:50 -0800

    Simple loop benchmark.

---
+++ LoopBenchmark/Benchmark.cs
@@ -0,0 +1,70 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Attributes;
+    using BenchmarkDotNet.Diagnosers;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+
+    public class SomeObject
+    {
+        public SomeObject(int val)
+        {
+            this.Val = val;
+        }
+
+        public int Val { get; }
+    }
+
+    [MemoryDiagnoser]
+    public class Benchmark
+    {
+        BlockingCollection<string> _logbuffer = new BlockingCollection<string>();
+
+        [Params(100, 10_000, 100_000, 1_000_000)]
+        public int Iterations { get; set; }
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+        }
+
+        [Benchmark]
+        public long WithList()
+        {
+            List<SomeObject> list = new List<SomeObject>() { new SomeObject(1), new SomeObject(1), new SomeObject(1) };
+            long result = 0;
+
+            for (int i = 0; i < this.Iterations; i++)
+            {
+                foreach (SomeObject so in list)
+                {
+                    result += so.ToString().GetHashCode();
+                }
+            }
+
+            return result;
+        }
+
+        [Benchmark]
+        public long WithDict()
+        {
+            Dictionary<string, SomeObject> dict = new Dictionary<string, SomeObject>();
+
+            dict["1"] = new SomeObject(1);
+            dict["2"] = new SomeObject(2);
+            dict["3"] = new SomeObject(3);
+
+            long result = 0;
+
+            for (int i = 0; i < this.Iterations; i++)
+            {
+                foreach (SomeObject so in dict.Values)
+                {
+                    result += so.ToString().GetHashCode();
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LoopBenchmark/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Test
+{
+    using BenchmarkDotNet.Running;
+    using Microsoft.Diagnostics.Tracing.Parsers.FrameworkEventSource;
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+}
