6d068287091a7caf06e332dc2cf2605774adc448
Mike Treit <mtreit@ntdev.microsoft.com>
2021-06-26 12:53:24 -0700

    Initial commit.

---
+++ BlobStorageSpeedTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace Test
+{
+    class BlobStorageSpeedTest
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        static void ProcessIt(string containerPath)
+        {
+
+        }
+    }
+}
