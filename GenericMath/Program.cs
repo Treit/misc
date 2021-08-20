e1f609c5e65e09d8ec0694105cf066794ff52acd
Mike Treit <mtreit@ntdev.microsoft.com>
2021-08-20 10:03:05 -0700

    Generic Math C# feature example.

---
+++ GenericMath/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+static T Add<T>(T left, T right)
+    where T : INumber<T>
+{
+    return left + right;
+}
+
+static T ParseInvariant<T>(string s)
+    where T : IParseable<T>
+{
+    return T.Parse(s, CultureInfo.InvariantCulture);
+}
+
+Console.Write("First number: ");
+var left = ParseInvariant<float>(Console.ReadLine());
+
+Console.Write("Second number: ");
+var right = ParseInvariant<float>(Console.ReadLine());
+
+Console.WriteLine($"Result: {Add(left, right)}");
