db49d3bb3ae3e0a327e0341cc0bb1b88310e0575
Mike Treit <mtreit@ntdev.microsoft.com>
2021-12-01 14:27:51 -0800

    Nested zip file extraction.

---
+++ ZipExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO.Compression;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Provide a zip file.");
+    return;
+}
+
+string filename = args[0];
+
+using var archive = ZipFile.Open(filename, ZipArchiveMode.Read);
+var currentArchive = archive;
+
+while (true)
+{
+    var last = currentArchive.Entries.Last();
+    if (!last.Name.EndsWith(".zip"))
+    {
+        break;
+    }
+
+    currentArchive = new ZipArchive(currentArchive.Entries.Last().Open());
+}
+
+var finalFile = currentArchive.Entries.Last();
+Console.WriteLine($"Terminated because we found {finalFile.Name}, which is not a zip file.");
+
+using var fs = finalFile.Open();
+using var destfs = new FileStream(finalFile.Name, FileMode.Create);
+fs.CopyTo(destfs);
+
+Console.WriteLine($"Wrote {finalFile.Name} to disk.");
+
---
+++ ZipExample/README.md
@@ -0,0 +1,7 @@
+# ZipExample
+Extracting a deeply nested zip file.
+
+Zip file original source, from `Human (or not)` on the C# discord:
+https://www.floriandalwigk.de/app/download/12930298726/geschenk.zip?t=1638217360
+
+
