f47c0e01e369f795a2270a8c8bc94318ac98c79e
Mike Treit <mtreit@ntdev.microsoft.com>
2022-10-05 16:13:57 -0700

    Repro of issue with AsParallel - stand-along program.

---
+++ AsParallelIssue/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1 || !int.TryParse(args[0], out int num))
+            {
+                Console.WriteLine("Provide a number of predicates to apply.");
+                return;
+            }
+
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine("About to run the test...");
+            IEnumerable<int> rows = new int[] { 1234 };
+            var predicates = new Func<int, bool>[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                predicates[i] = x => true;
+            }
+
+            foreach (var predicate in predicates)
+            {
+                rows = rows.AsParallel().Where(predicate);
+            }
+
+            var x = rows.ToList();
+            Console.WriteLine($"Test finished after {sw.ElapsedMilliseconds} ms.");
+        }
+    }
+}
---
+++ AsParallelIssue/README.md
@@ -0,0 +1,3 @@
+Shows an issue with AsParallel causing slowness.
+
+See https://github.com/dotnet/runtime/issues/76689
