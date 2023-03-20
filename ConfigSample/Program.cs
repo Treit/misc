c028267d528dbd907c1f0c017c613fa54a3b7c09
Mike Treit <mtreit@ntdev.microsoft.com>
2023-03-20 15:23:33 -0700

    Using .NET configuration providers in a simple console app.

---
+++ ConfigSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var builder =
+    new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .AddCommandLine(args);
+
+var config = builder.Build();
+var settings = config.GetRequiredSection("Logging").Get<Logging>();
+
+if (settings == null)
+{
+    Console.WriteLine("Could not find required settings.");
+    return;
+}
+
+Console.WriteLine(settings.LogLevel.Default);
+
+public sealed class Logging
+{
+    public LogLevelSetting LogLevel { get; init; } = null!;
+}
+
+public class LogLevelSetting
+{
+    public string Default { get; init; } = null!;
+    public string Microsoft { get; init; } = null!;
+}
