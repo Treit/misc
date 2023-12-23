2653c3c77371bb240d79b494901583614c36e2ad
Mike Treit <mtreit@ntdev.microsoft.com>
2023-12-23 15:22:47 -0800

    Reflection test app.

---
+++ ReflectionTest/TestApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+using TestLib;
+
+namespace Test
+{
+    class ReflectionTest : IObserver<DiagnosticListener>
+    {
+        public void DoTest()
+        {
+            var t = typeof(SimpleClass);
+            var method = t.GetMethods().Where(x => x.Name.Contains("DoSomething")).First();
+            var obj = Activator.CreateInstance(t);
+            method.Invoke(obj, new object?[] { this });
+        }
+
+        public void OnCompleted()
+        {
+            Console.WriteLine("OnCompleted");
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("OnError");
+        }
+
+        public void OnNext(DiagnosticListener value)
+        {
+            Console.WriteLine($"OnNext: {value.Name}");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var test = new ReflectionTest();
+            test.DoTest();
+        }
+    }
+}
