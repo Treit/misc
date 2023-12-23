11793394f92ba4cdd642b992ced9bcb74c62d0af
Mike Treit <mtreit@ntdev.microsoft.com>
2023-12-23 15:25:03 -0800

    Test lib

---
+++ ReflectionTest/TestLib/SimpleClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+namespace TestLib;
+
+public class SimpleClass
+{
+    public void DoSomething(IObserver<DiagnosticListener> observer)
+    {
+        observer.OnNext(new DiagnosticListener("something"));
+    }
+}
