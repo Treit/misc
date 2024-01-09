fef87ae4d55e8489cd9e69ca61ac3fa59550d8ff
Mike Treit <mtreit@ntdev.microsoft.com>
2024-01-09 12:15:34 -0800

    Add a variant that bumps the thread pool min threads count.

--- TaskRunDemo/Program.cs
+++ TaskRunDemo/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System.Diagnostics;
 
-ThreadPool.SetMinThreads(100, 100);
-
 await TestA();
 Console.WriteLine();
 
@@ -13,6 +11,10 @@ Console.WriteLine();
 
 await TestD();
 Console.WriteLine();
+
+await TestE();
+Console.WriteLine();
+
 static async Task TestA()
 {
     Console.WriteLine("TestA starting.");
@@ -147,4 +149,41 @@ static async Task TestD()
     resetEvent.Set();
     await Task.WhenAll(tasks);
     Console.WriteLine($"TestD done waiting for all tasks to finish after {sw.ElapsedMilliseconds} total ms.");
+}
+
+static async Task TestE()
+{
+    ThreadPool.SetMinThreads(100, 100);
+
+    Console.WriteLine("TestE starting.");
+    var sw = Stopwatch.StartNew();
+    var resetEvent = new ManualResetEvent(false);
+
+    var tasks = new List<Task<int>>();
+    for (int i = 0; i < 20; i++)
+    {
+        var lambda = async () =>
+        {
+            await Task.Yield();
+
+            // Now simulate doing some async work, such as making a network call.
+            await Task.Delay(500);
+
+            // Simulates doing some expensive synchronous work, such as an
+            // expensive CPU-bound computation.
+            Thread.Sleep(500);
+
+            resetEvent.WaitOne();
+
+            return Random.Shared.Next();
+        };
+
+        tasks.Add(lambda());
+    }
+
+    Console.WriteLine($"TestE doing some other work now after {sw.ElapsedMilliseconds} ms.");
+    await Task.Delay(100);
+    resetEvent.Set();
+    await Task.WhenAll(tasks);
+    Console.WriteLine($"TestE done waiting for all tasks to finish after {sw.ElapsedMilliseconds} total ms.");
 }
