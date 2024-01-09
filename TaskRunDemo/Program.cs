5fcaa60109d2ab4e09455c0a9b26ccd0def88ffe
Mike Treit <mtreit@ntdev.microsoft.com>
2024-01-09 11:40:09 -0800

    Task.Run and Task.Yield to avoid blocking the calling thread demo.

---
+++ TaskRunDemo/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Diagnostics;
+
+await TestA();
+Console.WriteLine();
+
+await TestB();
+Console.WriteLine();
+
+await TestC();
+Console.WriteLine();
+
+static async Task TestA()
+{
+    Console.WriteLine("TestA starting.");
+    var sw = Stopwatch.StartNew();
+    var resetEvent = new ManualResetEvent(false);
+
+    var tasks = new List<Task<int>>();
+    for (int i = 0; i < 20; i++)
+    {
+        var lambda = async () =>
+        {
+            // Simulates doing some expensive synchronous work, such as an
+            // expensive CPU-bound computation.
+            Thread.Sleep(500);
+
+            // Now simulate doing some async work, such as making a network call.
+            await Task.Delay(500);
+
+            resetEvent.WaitOne();
+
+            return Random.Shared.Next();
+        };
+
+        tasks.Add(lambda());
+    }
+
+    Console.WriteLine($"TestA doing some other work now after {sw.ElapsedMilliseconds} ms.");
+    await Task.Delay(100);
+    resetEvent.Set();
+    await Task.WhenAll(tasks);
+    Console.WriteLine($"TestA done waiting for all tasks to finish after {sw.ElapsedMilliseconds} total ms.");
+}
+
+static async Task TestB()
+{
+    Console.WriteLine("TestB starting.");
+    var sw = Stopwatch.StartNew();
+    var resetEvent = new ManualResetEvent(false);
+
+    var tasks = new List<Task<int>>();
+    for (int i = 0; i < 20; i++)
+    {
+        var task = Task.Run(async () =>
+        {
+            // Simulates doing some expensive synchronous work, such as an
+            // expensive CPU-bound computation.
+            Thread.Sleep(500);
+
+            // Now simulate doing some async work, such as making a network call.
+            await Task.Delay(500);
+
+            resetEvent.WaitOne();
+
+            return Random.Shared.Next();
+        });
+
+        tasks.Add(task);
+    }
+
+    Console.WriteLine($"TestB doing some other work now after {sw.ElapsedMilliseconds} ms.");
+    await Task.Delay(100);
+    resetEvent.Set();
+    await Task.WhenAll(tasks);
+    Console.WriteLine($"TestB done waiting for all tasks to finish after {sw.ElapsedMilliseconds} total ms.");
+}
+
+static async Task TestC()
+{
+    Console.WriteLine("TestA starting.");
+    var sw = Stopwatch.StartNew();
+    var resetEvent = new ManualResetEvent(false);
+
+    var tasks = new List<Task<int>>();
+    for (int i = 0; i < 20; i++)
+    {
+        var lambda = async () =>
+        {
+            await Task.Yield();
+
+            // Simulates doing some expensive synchronous work, such as an
+            // expensive CPU-bound computation.
+            Thread.Sleep(500);
+
+            // Now simulate doing some async work, such as making a network call.
+            await Task.Delay(500);
+
+            resetEvent.WaitOne();
+
+            return Random.Shared.Next();
+        };
+
+        tasks.Add(lambda());
+    }
+
+    Console.WriteLine($"TestC doing some other work now after {sw.ElapsedMilliseconds} ms.");
+    await Task.Delay(100);
+    resetEvent.Set();
+    await Task.WhenAll(tasks);
+    Console.WriteLine($"TestC done waiting for all tasks to finish after {sw.ElapsedMilliseconds} total ms.");
+}
