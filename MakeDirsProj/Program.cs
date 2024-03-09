36c3c178e1cf0c7e0a59acc69bd991c9f2160093
Mike Treit <mtreit@ntdev.microsoft.com>
2024-03-09 12:43:06 -0800

    Tool to create dirs.proj files.

---
+++ MakeDirsProj/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+var pattern = args is ["--dirs", ..] ? @"dirs\.proj$" : @".+\.csproj$|dirs\.proj$";
+var targetFile = "dirs.proj";
+
+if (File.Exists(targetFile))
+{
+    File.Delete(targetFile);
+}
+
+var files =
+    Directory.EnumerateFiles(".", "*.*", new EnumerationOptions { RecurseSubdirectories = true })
+    .Where(f => Regex.IsMatch(f, pattern));
+
+using var sw = new StreamWriter(targetFile);
+sw.WriteLine("""<Project Sdk="Microsoft.Build.Traversal">""");
+sw.WriteLine("  <ItemGroup>");
+
+foreach (var file in files)
+{
+    var path = Path.GetRelativePath(".", file);
+    if (path != "dirs.proj" && (pattern.Contains("csproj") || path.Count(c => c == '\\') is 1))
+    {
+        sw.WriteLine($"""    <ProjectFile Include="{path}" />""");
+    }
+}
+
+sw.WriteLine("  </ItemGroup>");
+sw.WriteLine("</Project>");
