6dff6e37275d446222a34a7ee1b293791b2b584d
Mike Treit (from Dev Box) <mtreit@ntdev.microsoft.com>
2024-03-09 17:25:05 -0800

    Unicode issue.

---
+++ UnicodeIssue/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Test
+{
+    class Program
+    {
+        private static readonly Dictionary<string, int> dict = new Dictionary<string, int>
+        {
+            {"intlv3_pok�mon", 1216},
+            {"intlv3_pok�mon", 1248}
+        };
+
+        static void Main(string[] args)
+        {
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
