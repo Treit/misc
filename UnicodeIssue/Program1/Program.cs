11f8e745b4ae056f885fe305101cc94999879499
Mike Treit <mtreit@ntdev.microsoft.com>
2024-03-09 17:59:21 -0800

    Add both versions of the brokens strings.

---
+++ UnicodeIssue/Program1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Test
+{
+    class Program
+    {
+        private static readonly Dictionary<string, int> dict = new Dictionary<string, int>
+        {
+            {"intlv3_pok�mon", 1216},
+            {"intlv3_pok�mon", 1248}
+        };
+
+        private static readonly Dictionary<string, int> dict2 = new Dictionary<string, int>
+        {
+            {"intlv3_pokã©mon", 1216},
+            {"intlv3_pokémon", 1248}
+        };
+
+
+        static void Main(string[] args)
+        {
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
