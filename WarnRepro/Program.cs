535e9d9aa51e7f93cb07e64714d22f0d1a562312
Mike Treit <mtreit@ntdev.microsoft.com>
2024-07-19 14:49:54 -0700

    Repro analyzers being pulled in with VS threading package.

---
+++ WarnRepro/Program.cs
@@ -0,0 +1,12 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    private static async Task LetMeNameThisWhateverIWantIDontCareAboutYourStupidConventions()
+    {
+        await Task.Delay(1000);
+    }
+}
