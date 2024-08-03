d57a52e06dd34a293f52829b6b3a60863b84f89c
Mike Treit <mtreit@ntdev.microsoft.com>
2024-08-03 14:16:00 -0700

    Test if code analysis flags unnecessary ToList or ToArray calls.

---
+++ CodeAnalysisTest/Program.cs
@@ -0,0 +1,25 @@
+﻿if (args.Length == 0)
+{
+    Console.WriteLine($"Provide an input string.");
+    return;
+}
+
+var str = args[0];
+
+if (str.Split('-').ToList().Count == 2)
+{
+    Console.WriteLine($"Input matched.");
+}
+else
+{
+    Console.WriteLine($"Input did not match.");
+}
+
+if (str.Split('-').ToArray().Length == 2)
+{
+    Console.WriteLine($"Input matched.");
+}
+else
+{
+    Console.WriteLine($"Input did not match.");
+}
